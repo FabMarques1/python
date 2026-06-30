051f595f1f27fc3f1f53280e9b458d38bc76ab3b
Fabricio Henrique Marques Olimpio <fabricioolimpio734@gmail.com>
2026-06-30 16:37:07 -0300

    Mudança de estrutura

--- manipulacao-de-dados/main.py
+++ manipulacao-de-dados/main.py
@@ -24,7 +24,7 @@ print("-------- BEM VINDO A SUA CONTA --------")
 
 while True:
     print("---------------------------------------")
-    print("1 - Ver seus dados locais\n2 - Editar seu nome\n3 - Editar seu CPF\n4 - Editar sua idade\n5 - Salvar informações\n6 - Sair\n")
+    print("1 - Ver perfil\n2 - Editar configurações\n3 - Sair\n")
 
     if save == False:
         print("(Dados modificados não salvos...)\n")
@@ -43,37 +43,78 @@ while True:
             print(f"Idade: {local_info['idade']}")
 
         case 2:
-            print("-------- EDIÇÃO --------")
-            local_info["nome"] = input("Edite seu nome: ")
+            configuracoes(save)
 
-            save = False
-        
         case 3:
-            print("-------- EDIÇÃO --------")
-            local_info["cpf"] = input("Edite seu CPF: ")
-
-            save = False
-
-        case 4:
-            print("-------- EDIÇÃO --------")
-            print("AVISO: Ao usar a data de nascimento como identificador de idade, pode ocorrer de mostrar a idade que você faria este ano caso não tenha feito.\n")
-            anoNasc = int(input("Digite o ano de nascimento: "))
-            local_info["idade"] = (anoAtual - anoNasc)
-
-            save = False
-        
-        case 5:
-            print("-------- SALVANDO --------")
-            print("Dados salvos com sucesso!")
-            file = open("dados.json", "w", encoding="utf-8")
-            save_data = json.dump(local_info, file)
-            file.close()
-
-            save = True
-
-        case 6:
             print("Obrigado por testar!")
             break
 
         case _:
             print("ERRO!\nEssa opção não existe. Tem certeza que digitou corretamente?")
+
+
+
+    # Editar o perfil através de uma função
+
+    def configuracoes(save_var):
+        print("-------- EDIÇÃO DE CONTA --------")
+        print("Selecione a opção desejada:")
+
+        while True:
+            
+            print("---------------------------------------")
+            print("1 - Editar nome\n2 - Editar CPF\n3 - Editar idade\n4 - Salvar informações\n5 - Voltar\n")
+
+            
+            if save_var == False:
+                print("(Dados modificados não salvos...)\n")
+
+            # Solicito para o usuário a escolha de uma opção
+            conta = int(input("Digite sua escolha: "))
+            os.system("cls")
+
+            
+            match conta:
+                case 1:
+                    print("-------- EDIÇÃO DE CONTA --------")
+                    local_info["nome"] = input("Edite seu nome: ")
+
+                    os.system("cls")
+                    print("-------- EDIÇÃO DE CONTA --------")
+                    print("Edição feita com sucesso!")
+
+                    save_var = False
+                
+                case 2:
+                    print("-------- EDIÇÃO DE CONTA --------")
+                    local_info["cpf"] = input("Edite seu CPF: ")
+
+                    os.system("cls")
+                    print("-------- EDIÇÃO DE CONTA --------")
+                    print("Edição feita com sucesso!")
+
+                    save_var = False
+
+                case 3:
+                    print("-------- EDIÇÃO DE CONTA --------")
+                    print("AVISO: Ao usar a data de nascimento como identificador de idade, pode ocorrer de mostrar a idade que você faria este ano caso não tenha feito.\n")
+                    anoNasc = int(input("Digite o ano de nascimento: "))
+                    local_info["idade"] = (anoAtual - anoNasc)
+
+                    os.system("cls")
+                    print("-------- EDIÇÃO DE CONTA --------")
+                    print("Edição feita com sucesso!")
+
+                    save_var = False
+                
+                case 4:
+                    print("-------- EDIÇÃO DE CONTA --------")
+                    print("Dados salvos com sucesso!")
+                    with open("dados.json", "w", encoding="utf-8") as file:
+                        save_data = json.dump(local_info, file)
+                    
+                    save_var = True
+
+                case 5:
+                    break
+
