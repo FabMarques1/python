8b696a99e97ae6b701c3be854f932afa08d0f28d
Fabricio Henrique Marques Olimpio <fabricioolimpio734@gmail.com>
2026-07-01 13:59:19 -0300

    Configurações e ajustes
    Em desenvolvimento a parte de configurações do arquivo, bem como alterar a estrutura e corrigir erros lógicos conforme o decorrer do arquivo. Commit feito para salvar modificações em andamento.

--- manipulacao-de-dados/func.py
+++ manipulacao-de-dados/func.py
@@ -18,7 +18,7 @@ def register(nome, email, idade):
 
             if char == ']':
                 file.seek(pos - 2)
-                file.write(f',\n\t{data}\n]')
+                file.write('},\n\t' + data + '\n]')
 
                 break
 
@@ -37,6 +37,8 @@ def login(json_data, email):
 def menu(session_var, data):
     os.system("cls")
 
+    save = True
+
     if session_var == True:
         print("---------------------------------------")
         print("-------- BEM VINDO A SUA CONTA --------")
@@ -44,6 +46,9 @@ def menu(session_var, data):
         while True:
             print("---------------------------------------")
             print("1 - Ver suas informações\n2 - Editar perfil\n3 - Logout\n")
+
+            if save == False:
+                print("AVISO: Mudanças no perfil não foram salvas.")
 
             op = int(input("Digite sua escolha: "))
             os.system("cls")
@@ -57,7 +62,7 @@ def menu(session_var, data):
                     print(f"Idade: {data['idade']}")
 
                 case 2:
-                    print("Migrando configurações...")
+                    configuracoes(save, data)
 
                 case 3:
                     data["nome"] = "?"
@@ -75,3 +80,66 @@ def menu(session_var, data):
     
     else:
         print("ERRO: Não foi possível entrar na sua conta. Você não está logado!")
+
+
+def configuracoes(save_var, data):
+        print("-------- EDIÇÃO DE CONTA --------")
+        print("Selecione a opção desejada:")
+
+        while True:
+            
+            print("---------------------------------------")
+            print("1 - Editar nome\n2 - Editar e-mail\n3 - Editar idade\n4 - Salvar informações\n5 - Voltar\n")
+
+            
+            if save_var == False:
+                print("AVISO: Mudanças no perfil não foram salvas.")
+
+            # Solicito para o usuário a escolha de uma opção
+            conta = int(input("Digite sua escolha: "))
+            os.system("cls")
+
+            
+            match conta:
+                case 1:
+                    print("-------- EDIÇÃO DE CONTA --------")
+                    data["nome"] = input("Edite seu nome: ")
+
+                    os.system("cls")
+                    print("-------- EDIÇÃO DE CONTA --------")
+                    print("Edição feita com sucesso!")
+
+                    save_var = False
+                
+                case 2:
+                    print("-------- EDIÇÃO DE CONTA --------")
+                    data["email"] = input("Edite seu e-mail: ")
+
+                    os.system("cls")
+                    print("-------- EDIÇÃO DE CONTA --------")
+                    print("Edição feita com sucesso!")
+
+                    save_var = False
+
+                case 3:
+                    print("-------- EDIÇÃO DE CONTA --------")
+                    print("AVISO: Ao usar a data de nascimento como identificador de idade, pode ocorrer de mostrar a idade que você faria este ano caso não tenha feito.\n")
+                    anoNasc = int(input("Digite o ano de nascimento: "))
+                    data["idade"] = (anoAtual - anoNasc)
+
+                    os.system("cls")
+                    print("-------- EDIÇÃO DE CONTA --------")
+                    print("Edição feita com sucesso!")
+
+                    save_var = False
+                
+                case 4:
+                    print("-------- EDIÇÃO DE CONTA --------")
+                    print("Dados salvos com sucesso!")
+                    with open("dados.json", "w", encoding="utf-8") as file:
+                        save_data = json.dump(data, file)
+                    
+                    save_var = True
+
+                case 5:
+                    break
